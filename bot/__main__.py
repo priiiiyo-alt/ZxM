d37aff9b21de2527bb04c9c14c1afaf8645aa186
priiiiyo-alt <85226909+priiiiyo-alt@users.noreply.github.com>
2022-08-06 02:31:36 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -64,10 +64,10 @@ def stats(update, context):
 def start(update, context):
     buttons = ButtonMaker()
 
-    buttons.buildbutton("Report Group", "https://t.me/Mltb_chat_unofficial")
-    buttons.buildbutton("Repo", "https://github.com/Dawn-India/Z-Mirror")
-    buttons.buildbutton("Mirror Group", "https://t.me/z_mirror")
-    buttons.buildbutton("Owner", "https://t.me/dawn_in")
+    buttons.buildbutton("Report Group", "https://t.me/tmlb_updates")
+    buttons.buildbutton("Repo", "https://github.com/priiiiyo/tg-mirror-leech-bot")
+    buttons.buildbutton("Mirror Group", "https://t.me/PriyoS_Mirror")
+    buttons.buildbutton("Owner", "https://t.me/priiiiyo")
 
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
@@ -169,7 +169,7 @@ help_string_telegraph = f'''<br>
 '''
 
 help = telegraph.create_page(
-        title='Z-Mirror-Bot Help',
+        title='PriyoS-Mirror-Bot Help',
         content=help_string_telegraph,
     )["path"]
 
